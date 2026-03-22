8b0b5091de5809bae729f77ca587e7f6c93c8fdf
bikash-backendcraftsman <bikash.backendcraftsman@gmail.com>
2026-03-22 21:06:04 +0530

    git commit -m "Stream Operation question practice added : Date - 22 March 2026"

--- StreamOperation/src/main/java/com/technostack/stream/string/findSecondLargestElementInArray.java
+++ StreamOperation/src/main/java/com/technostack/stream/string/findSecondLargestElementInArray.java
@@ -1,7 +1,26 @@
 package com.technostack.stream.string;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public class findSecondLargestElementInArray {
     public static void main(String[] args) {
+        int[] array = {12,55,95,3,200,99};
+       int n =  filterSecondLargest(array);
+        System.out.println(n);
+    }
 
+    private static int filterSecondLargest(int[] array) {
+        if(array.length == 0){
+            return -1;
+        }
+
+       return Arrays.stream(array)
+                .distinct()
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst().orElse(0);
     }
 }
